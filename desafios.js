245a4da7da1b25afa26c1726b3eb2dbd008d938d
Lipeoe <lipaosantos671@gmail.com>
2024-01-18 15:35:00 -0300

    Initial commit

---
+++ desafios.js
@@ -0,0 +1,23 @@
+alert ('Bem-vindo!!!')
+
+let nome = 'Lua';
+alert('Nome: ' + nome);
+let idade = '25';
+alert('Idade: ' + idade);
+let numeroDeVendas = '50';
+alert('Número de vendas: ' + numeroDeVendas);
+let saldoDisponivel = '5000';
+alert('Saldo disponível: R$' + saldoDisponivel);
+
+alert ('Erro! Preencha todos os campos');
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+
+let = prompt('Digite seu nome:');
+let idade1 = prompt('Digite sua idade:');
+
+let idadeHabilitacao = '18'
+if (idade1 >= idadeHabilitacao) {
+    alert('Pode tirar habilitação.');
+} else {
+    alert('Não pode tirar habilitação.');
+}
